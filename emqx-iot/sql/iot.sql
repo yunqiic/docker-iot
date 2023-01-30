a75ed28678101f202f07bb6c0ed50209ba81502f
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-01-30 16:52:28 +0800

    update

---
+++ emqx-iot/sql/iot.sql
@@ -0,0 +1,45 @@
+SET NAMES utf8mb4;
+use iot;
+
+-- mqtt_client 存储设备在线状态
+DROP TABLE IF EXISTS `mqtt_client`;
+CREATE TABLE `mqtt_client` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `clientid` varchar(64) DEFAULT NULL,
+  `state` varchar(3) DEFAULT NULL, -- 在线状态 0 离线 1 在线
+  `node` varchar(100) DEFAULT NULL, -- 所属节点
+  `online_at` datetime DEFAULT NULL, -- 上线时间
+  `offline_at` datetime DEFAULT NULL, -- 下线时间
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `mqtt_client_idx` (`clientid`),
+  UNIQUE KEY `mqtt_client_key` (`clientid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- mqtt_sub 存储设备的主题订阅关系
+DROP TABLE IF EXISTS `mqtt_sub`;
+CREATE TABLE `mqtt_sub` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `clientid` varchar(64) DEFAULT NULL,
+  `topic` varchar(255) DEFAULT NULL,
+  `qos` int(3) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `mqtt_sub_idx` (`clientid`,`topic`(255),`qos`),
+  UNIQUE KEY `mqtt_sub_key` (`clientid`,`topic`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- mqtt_msg 存储 MQTT 消息
+DROP TABLE IF EXISTS `mqtt_msg`;
+CREATE TABLE `mqtt_msg` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `msgid` varchar(100) DEFAULT NULL,
+  `topic` varchar(1024) NOT NULL,
+  `sender` varchar(1024) DEFAULT NULL,
+  `node` varchar(60) DEFAULT NULL,
+  `qos` int(11) NOT NULL DEFAULT '0',
+  `retain` tinyint(2) DEFAULT NULL,
+  `payload` blob,
+  `arrived` datetime NOT NULL, -- 是否抵达（QoS > 0）
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
